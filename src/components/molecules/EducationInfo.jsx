1dadc25c4dc42ba7634d08121a71e9f232116487
camilo carvajal <camilocc597@gmail.com>
2023-09-24 22:40:02 -0500

    education info

---
+++ src/components/molecules/EducationInfo.jsx
@@ -0,0 +1,24 @@
+
+function EducationInfo() {
+    return (
+
+        <div>
+            <label>Profesional</label>
+            <ul>
+                <li>Ingeniería de sistemas</li>
+                <li>Tecnología de sistemas de informacion</li>
+            </ul>
+            <label>Cursos</label>
+            <ul>
+                <li>Herramientas para innovar</li>
+                <li>Fundamentos de la gestión del tiempo</li>
+                <li>Bases de datos generalidades y sistemas de gestión</li>
+                <li>Git going fast: One hour git crash course</li>
+                <li>Variables y estructuras de control en la programación orientada a objetos</li>
+                <li>Procesos para software de calidad</li>
+            </ul>
+        </div>
+    );
+}
+
+export default EducationInfo;
